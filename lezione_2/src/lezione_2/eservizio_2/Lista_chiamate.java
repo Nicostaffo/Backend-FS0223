cde666630cd2359062a4d4da8003a55c90859740
Nicostaffo <staffolani.niccolo1@outlook.com>
2023-06-27 16:31:48 +0200

    ciao

---
+++ lezione_2/src/lezione_2/eservizio_2/Lista_chiamate.java
@@ -0,0 +1,18 @@
+package lezione_2.eservizio_2;
+
+public class Lista_chiamate {
+int tempoChiamata;
+Sim utente1;
+int utente2;
+
+public Lista_chiamate(int tempoChiamata,Sim utente1, int utente2) {
+this.tempoChiamata = tempoChiamata;
+utente1.chiamate++;
+this.utente2 = utente2;
+this.utente1 = utente1;
+}
+public void showdati() {
+	System.out.println("durata call " + this.tempoChiamata);
+	System.out.println("utente chiamato " + this.utente2);
+}
+}
---
+++ niko/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author staff
+ *
+ */
+module niko {
+}
